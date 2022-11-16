e6f7957f2adea30b847fd607d32dbf382f95dc96
Simon Meyer <sy@2c358933-7dd2-456e-bfd6-b1b25cf1aa60.fritz.box>
2022-11-16 18:47:11 +0100

    redo meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <title>VOSS</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
       <body className="bg-neutral-50 dark:bg-neutral-800 dark:text-white">
         <Main />
         <NextScript />
