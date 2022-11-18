dcbd226aa2b777bcf41b960f2ffb16f5c8fdd2b6
smeycodes <9540040+smeycodes@users.noreply.github.com>
2022-11-18 22:53:07 +0100

    add api route for customer creation.

---
+++ db/migrations/20221118181923_customers.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex
+    .schema
+    .createTable('customers', table => {
+      table.increments('id').unsigned();
+      table.string('name').notNullable();
+      table.string('email').notNullable().unique();
+      table.datetime('time_created', { useTz: true }).notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex
+    .schema
+    .dropTableIfExists('users');
+};
---
+++ pages/api/newcustomer.ts
@@ -0,0 +1,27 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { sessionParameters } from '@lib/session';
+import { UserModel } from '@models/user/user';
+import type { NextApiRequest, NextApiResponse } from 'next/types';
+
+async function newCustomerRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { email, password } = await req.body;
+  const users = new UserModel();
+
+  try {
+    let user = await users.login(email, password);
+    if(user) {
+      req.session.user = user;
+      req.session.user.isLoggedIn = true;
+      await req.session.save();
+
+      user = req.session.user;
+      res.status(200).json({ success: true, user });
+    }
+
+  } catch(e) {
+    console.log(e);
+    res.status(200).json({ success: false });
+  }
+}
+
+export default withIronSessionApiRoute(newCustomerRoute, sessionParameters);
